865c92f9ddf5227c89e742e9e3ed8c5c245a2754
“Taniabhattacharya” <1234taniabhattacharya@gmail.com>
2023-05-12 15:05:06 +0530

    CODE AND README FILE UPDATED

--- LAB5/lab5.java
+++ LAB5/lab5.java
@@ -1,72 +1,79 @@
 import java.util.Scanner;
 
 public class Lab5 {
+    private static String encryptMessage(String messageToEncrypt, char[] plain , char[] cypher) {
+        messageToEncrypt = messageToEncrypt.toUpperCase();
+        char [] messageChars = messageToEncrypt.toCharArray();
+        for (int i = 0; i < messageChars.length; i++) {
+            if (messageChars[i] != ' '){
+            for (int j = 0; j < plain.length; j++) {
+                if (messageChars[i] == plain[j]) {
+                    messageChars[i] = cypher[j];
+                    break;  
+                }
+            }
+            }
+        }
+        String encrypted_message = String.valueOf(messageChars);
+
+        return encrypted_message;
+    }
+
+
+    private static String decryptMessage(String messageToEncrypt, char[] plain , char[] cypher) {
+        messageToEncrypt = messageToEncrypt.toUpperCase();
+        char [] messageChars = messageToEncrypt.toCharArray();
+        for (int i = 0; i < messageChars.length; i++) {
+            
+            for (int j = 0; j < plain.length; j++) {
+                if (messageChars[i] == plain[j]) {
+                    messageChars[i] = cypher[j];
+                    break;  
+                }
+            }
+        }
+        String decrypted_message = String.valueOf(messageChars);
+
+        return decrypted_message;
+
+    }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int ch;
-        String mgs;
-
+        Scanner scanner = new Scanner(System.in);
+        int choice;
         do {
-            System.out.println("\nPlease enter your choice:");
-            System.out.println("1. Encrypt a message");
-            System.out.println("2. Decrypt a message");
+            System.out.println("Enter your choice:");
+            System.out.println("1. Encrypt message");
+            System.out.println("2. Decrypt message");
             System.out.println("3. Exit");
+            choice = Integer.parseInt(scanner.nextLine());
+            
+            //scanner.nextLine(); 
+            char[] plain = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+            char[] cypher = {'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W'};
 
-            ch = sc.nextInt();
-            sc.nextLine();
 
-            switch (ch) {
+            switch (choice) {
                 case 1:
-                System.out.println("Enter the message that you want to encrypt:");
-                    mgs = sc.nextLine();
-                    System.out.println("Encrypted message: " + ept(mgs));
+                    System.out.println("Enter message to encrypt:");
+                    String messageToEncrypt = scanner.nextLine();
+                    String encryptedMessage = encryptMessage(messageToEncrypt, plain , cypher);
+                    System.out.println("Encrypted message: " + encryptedMessage);
                     break;
                 case 2:
-                    System.out.println("Enter the message that you want to decrypt:");
-                    mgs = sc.nextLine();
-                    System.out.println("Decrypted message: " + dcpt(mgs));
+                    System.out.println("Enter message to decrypt:");
+                    String messageToDecrypt = scanner.nextLine();
+                    String decryptedMessage = decryptMessage(messageToDecrypt, plain , cypher);
+                    System.out.println("Decrypted message: " + decryptedMessage);
                     break;
                 case 3:
-                    System.exit(0);
+                    System.out.println("Exiting program...");
                     break;
                 default:
-                    System.out.println("Invalid choice, please try again.");
+                    System.out.println("Invalid choice. Please enter again.");
+                    break;
             }
-        } while (true);
-    }
-
-    public static String ept(String mgs) {
-        String enptmgs = "";
-        mgs = mgs.toUpperCase();
-for (int i = 0; i < mgs.length(); i++) {
-            char c = mgs.charAt(i);
-
-            if (c >= 'A' && c <= 'Z') {
-                c = (char) ('X' + (c - 'A'));
-                if (c > 'Z') {
-                    c = (char) (c - 26);
-                }
-            }
-            enptmgs += c;
-        }
-        return enptmgs;
-    }
-
-    public static String dcpt(String mgs) {
-        String dcptmgs = "";
-        mgs = mgs.toUpperCase();
-
-        for (int i = 0; i < mgs.length(); i++) {
-            char c = mgs.charAt(i);
-            if (c >= 'A' && c <= 'Z') {
-                c = (char) ('A' + (c - 'X'));
-                if (c < 'A') {
-                    c = (char) (c + 26);
-                }
-            }
-            dcptmgs += c;
-        }
-        return dcptmgs;
+        } while (choice != 3); 
+    
     }
 }
--- LAB5/readme.md
+++ LAB5/readme.md
@@ -120,4 +120,28 @@ Ciphertext: QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD
 
 You are supposed to create a menu-driven program that can encrypt or decrypt a message that the user wishes to transfer.
 Your program should have two functions, one for encryption and another for decryption. Each function will accept a string message and return the encrypted or decrypted message.
+---------------------------------------------------------------------------------------------------------------
 
+THE OUTPUTVOF THIS PROGRAM IS:
+Enter your choice:
+1. Encrypt message
+2. Decrypt message
+3. Exit
+1
+Enter message to encrypt:
+abc
+Encrypted message: XYZ
+Enter your choice:
+1. Encrypt message
+2. Decrypt message
+3. Exit
+2
+Enter message to decrypt:
+xyz
+Decrypted message: UVW
+Enter your choice:
+1. Encrypt message
+2. Decrypt message
+3. Exit
+3
+Exiting program...
